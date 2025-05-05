f7d28f46523a34d9cdcf318a21b3e328087bbe63
starky976 <black456heat@gmail.com>
2025-05-05 14:33:28 +0900

    follow, unfollow

--- resources/views/commons/link_items.blade.php
+++ resources/views/commons/link_items.blade.php
@@ -1,11 +1,11 @@
 @if (Auth::check())
     {{-- ユーザー一覧ページへのリンク --}}
-    <li><a class="link link-hover" href="#">Users</a></li>
+    <li><a class="link link-hover" href="{{ route('users.index') }}">Users</a></li>
     {{-- ユーザー詳細ページへのリンク --}}
-    <li><a class="link link-hover" href="#">{{ Auth::user()->name }}&#39;s profile</a></li>
+    <li><a class="link link-hover" href="{{ route('users.show',$user->id) }}">{{ Auth::user()->name }}&#39;s profile</a></li>
     <li class="divider lg:hidden"></li>
     {{-- ログアウトへのリンク --}}
-    <li><a class="link link-hover" href="#" onclick="event.preventDefault();this.closest('form').submit();">Logout</a></li>
+    <li><a class="link link-hover" href="{{ route('logout') }}" onclick="event.preventDefault();this.closest('form').submit();">Logout</a></li>
 @else
     {{-- ユーザー登録ページへのリンク --}}
     <li><a class="link link-hover" href="{{ route('register') }}">Signup</a></li>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UsersController;
 use App\Http\Controllers\MicropostsController;
+use App\Http\Controllers\UserFollowController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,11 +20,14 @@ Route::get('/', [MicropostsController::class, 'index']);
 Route::get('/dashboard', [MicropostsController::class, 'index'])->middleware(['auth'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::prefix('users/{id}')->group(function () {
+        Route::post('follow', [UserFollowController::class, 'store'])->name('user.follow');
+        Route::delete('unfollow', [UserFollowController::class, 'destroy'])->name('user.unfollow');
+        Route::get('followings', [UsersController::class, 'followings'])->name('users.followings');
+        Route::get('followers', [UsersController::class, 'followers'])->name('users.followers');
+    });
     Route::resource('users', UsersController::class, ['only' => ['index', 'show']]);
     Route::resource('microposts', MicropostsController::class, ['only' => ['store', 'destroy']]);
-    // Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    // Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    // Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 
